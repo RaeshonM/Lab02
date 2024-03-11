3b8ae31ec48f5da1fd9ba87b080300b313b068c6
JMSGM <jmsgm123@gmail.com>
2024-03-10 20:31:52 -0700

    update hello

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
 		System.out.println("303757870 - Raeshon Minnifield");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("302882476 - Joseph Mathew Sagum");
 		System.out.println("303059770 - Adrian Perez");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
